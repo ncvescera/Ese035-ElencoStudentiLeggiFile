4e0d7ce705bde1c9bad60dbcde208401abfc61dc
Nicolò Vescera <slayercandy@icloud.com>
2015-04-22 17:03:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,44 @@
 #include <stdlib.h>
 
 // Definisci le costanti
-
+#define N 2
+#define MAX_STRLEN 30
 // Definisci il tipo di dato struct s_studente
-
+struct s_studente{
+    char nome[MAX_STRLEN+1];
+    char cognome[MAX_STRLEN+1];
+    int eta;
+    char classe[MAX_STRLEN+1];
+};
 // Definisci studente come struct s_studente
-
+typedef struct s_studente studente;
 
 int main(int argc, char** argv) {
     // Dichiara le variabili locali
-    
+    FILE *puntafile;
+    int i;
+    char fileName[MAX_STRLEN+1];
+    studente elenco[N];
     // Codice per la richiesta del nome del file
-    
+    printf("Inserisci il nome del file che voi aprire: ");
+    scanf("%s",fileName);
     // Codice per l'apertura del file e la lettura
-    
-    // Codice per la stampa a schermo
-    
+    puntafile=fopen(fileName,"rb");
+    if(puntafile==NULL)
+        printf("Errore! Impossibile aprire il file!");
+    else{
+        printf("---Stampa---\n");
+        while(!feof(puntafile)){
+            fread(elenco,sizeof(studente),N,puntafile);
+            // Codice per la stampa a schermo
+            for(i=0;i<N;i++){
+		printf("Nome: %s\n",elenco[i].nome);
+                printf("Cognome: %s\n",elenco[i].cognome);
+                printf("Età: %d\n",elenco[i].eta);
+                printf("Classe: %s\n\n",elenco[i].classe);
+            }
+        }
+    }
+    fclose(puntafile);
     return (EXIT_SUCCESS);
 }
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=ese035-elencostudentileggifile
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese035-elencostudentileggifile
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ese035-elencostudentileggifile
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=ese035-elencostudentileggifile.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese035-elencostudentileggifile.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ese035-elencostudentileggifile.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
